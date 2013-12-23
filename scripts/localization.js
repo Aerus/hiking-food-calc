a9d4d5645081161b6306498193af90009dfe8b28
Serhii Kotyk <kotykserg@gmail.com>
2013-12-23 10:43:52 +0200

    First commit. working on prototype

---
+++ scripts/localization.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Serhii_Kotyk on 12/20/13.
+ */
+var Localization = {
+    PAGE_TITLE: "Заголовок",
+
+    DAYS_LABLE: "Дни:",
+    DAYS_PLACEHOLDER: "К-во дней",
+
+    PEOPLE_LABLE: "Группа:",
+    PEOPLE_PLACEHOLDER: "К-во людей"
+
+};
